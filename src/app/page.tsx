a7d5b83bbeaa102fd0c27c0845b5f99c82dee336
moritzWa <wallawitsch@gmail.com>
2024-04-07 18:03:16 -0700

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 import { MainComponent } from "@/Components/MainComponent";
 
+// test
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
