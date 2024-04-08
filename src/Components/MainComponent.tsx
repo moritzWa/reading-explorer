7a2cf0dfd5be35c769b02a0794798f764a039b20
moritzWa <wallawitsch@gmail.com>
2024-04-07 18:52:12 -0700

    start article page

--- src/Components/MainComponent.tsx
+++ src/Components/MainComponent.tsx
@@ -5,10 +5,13 @@ import {
   DataForSEOBacklinkItem,
   DataForSEOBacklinkResponse,
 } from "@/trpc/types";
+import Image from "next/image";
 import { useState } from "react";
+import Article from "./Article";
 
 export const MainComponent = () => {
   const [link, setLink] = useState("");
+  const [submitted, setSubmitted] = useState(false);
   const { data, isLoading, error, refetch } = trpc.getBackLinks.useQuery<
     DataForSEOBacklinkResponse[]
   >(link, {
@@ -17,6 +20,7 @@ export const MainComponent = () => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setSubmitted(true);
     await refetch();
   };
 
@@ -24,48 +28,48 @@ export const MainComponent = () => {
 
   return (
     <div>
+      <div className="flex justify-center mb-10">
+        <Image src="/dora-logo.svg" alt="Dora Logo" width={200} height={200} />
+      </div>
       <form onSubmit={handleSubmit}>
         <label htmlFor="input">
-          Link:
           <input
+            className="min-w-[300px] text-xl p-8 rounded-full"
             id="input"
             placeholder="Put your article link here"
             value={link}
             onChange={(e) => setLink(e.target.value)}
           />
         </label>
-        <button type="submit">Get Backlinks</button>
+        <button
+          className="bg-blue-500 ml-5 text-xl text-white p-8 rounded-full"
+          type="submit"
+        >
+          Get Backlinks
+        </button>
       </form>
 
-      {isLoading ? (
+      {submitted && isLoading ? (
         <div>Loading...</div>
       ) : error ? (
         <div>Error: {error.message}</div>
-      ) : !data ? (
+      ) : !data && submitted ? (
         <div>Loading data...</div>
-      ) : data && data[0].items && data[0].items.length === 0 ? (
+      ) : data && data[0] && data[0].items && data[0].items.length === 0 ? (
         <div>No backlinks found.</div>
-      ) : (
+      ) : submitted ? (
         <div>
           <h2>Backlinks:</h2>
-          <ul>
+          <div>
             {data &&
               data[0].items.map(
                 (item: DataForSEOBacklinkItem, index: number) => (
-                  <li key={index}>
-                    <a
-                      href={item.url_from}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {item.page_from_title}
-                    </a>
-                  </li>
+                  <Article key={index} item={item} />
                 )
               )}
-          </ul>
+          </div>
         </div>
-      )}
+      ) : null}
     </div>
   );
 };
